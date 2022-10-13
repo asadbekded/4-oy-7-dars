2208d1fda900da09a5cb3a0f79988971fdf7676b
asadbekded <asadbek171@gmail.com>
2022-10-13 09:33:32 +0500

    Add new change js

--- js/main.js
+++ js/main.js
@@ -4,26 +4,37 @@ const elList = document.querySelector('.js-list');
 books.forEach((film) => {
    newItem = document.createElement('li')
    newTitle = document.createElement('h4')
+   newImg = document.createElement('img')
    newTitl = document.createElement('h5')
    newSpan1 = document.createElement('span')
    newSpan2 = document.createElement('span')
    newText = document.createElement('p')
-   newNum = document.createElement('strong')
+   newSpan3 = document.createElement('span')
+   newSpan4 = document.createElement('span')
+   newLink = document.createElement('a')
 
-   newTitle.textContent = film.author
-   newTitl.textContent = film.country
-   newSpan1.textContent = film.language
-   newSpan2.textContent = film.pages
-   newText.textContent = film.title
-   newNum.textContent = film.year
+   newTitle.textContent = film.title.toUpperCase()
+   newImg.src = `./images/${film.imageLink}`;
+   newImg.style.width = "100%";
+   newImg.style.height = "350px";
+   newTitl.textContent = `Author: ${film.author}`
+   newSpan1.textContent = `Country: ${film.country}`
+   newSpan2.textContent = `Language: ${film.language}`
+   newSpan3.textContent = `Pages: ${film.pages}`
+   newSpan4.textContent = `Date: ${film.year} year`
+   newLink.textContent = 'Learn-more'
+   newLink.href = film.link
 
    elList.appendChild(newItem)
    newItem.appendChild(newTitle)
+   newItem.appendChild(newImg)
    newItem.appendChild(newTitl)
    newItem.appendChild(newSpan1)
    newItem.appendChild(newSpan2)
    newItem.appendChild(newText)
-   newItem.appendChild(newNum)
+   newItem.appendChild(newSpan3)
+   newItem.appendChild(newSpan4)
+   newItem.appendChild(newLink)
 })
 //------------------------------------------dark and red mode codes btn begin-----------------------------------------
 
